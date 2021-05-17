114d404fc5604d4d7f0ed8fe68944663759445bc
Andrew Allen <allenan@helium.com>
2021-05-17 14:44:27 -0400

    add cors policy

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const compression = require('compression')
+const cors = require('cors')
 const env = process.env.NODE_ENV || 'development'
 
 import indexRouter from './routes/indexRouter'
@@ -30,6 +31,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors())
 
 app.use('/', indexRouter)
 app.use('/api', apiRouter)
