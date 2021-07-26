8d01f3a8b32a5d7d74804832f88ba599b492fce5
Andrew Allen <allenan@helium.com>
2021-07-26 15:03:41 -0700

    add dc to hexes

--- src/jobs/generate_hexes.js
+++ src/jobs/generate_hexes.js
@@ -1,21 +1,51 @@
-const { groupBy, compact, mean, round } = require('lodash')
-const { setCache } = require('../helpers/cache')
+const { default: Client } = require('@helium/http')
+const { groupBy, compact, mean, round, flatten, sum } = require('lodash')
+const { setCache, getCache } = require('../helpers/cache')
 const { fetchAll } = require('../helpers/pagination')
+
+const client = new Client()
+
+const fetchDcByHotspot = async () => {
+  const dcByHotspot = {}
+
+  const scIndex = await getCache('scIndex')
+  const txnHashes = flatten(Object.values(scIndex))
+
+  for (let i = 0; i < txnHashes.length; i++) {
+    const hash = txnHashes[i];
+
+    const txn = await client.transactions.get(hash)
+
+    txn.stateChannel.summaries.forEach(({ client: hotspot, num_dcs: dc }) => {
+      if (dcByHotspot[hotspot]) {
+        dcByHotspot[hotspot] += dc
+      } else {
+        dcByHotspot[hotspot] = dc
+      }
+    })
+    
+  }
+
+  return dcByHotspot
+}
 
 const run = async () => {
   const hotspots = await fetchAll('/hotspots')
   const hotspotsByHex = groupBy(hotspots, 'location_hex')
+  const dcByHotspot = await fetchDcByHotspot()
 
   // ignore hotspots without location for the purpose of coverage
   delete hotspotsByHex.null
 
   const hexes = Object.keys(hotspotsByHex).map((hex) => {
     const rewardScales = compact(hotspotsByHex[hex].map((h) => h.reward_scale))
+    const dc = sum(hotspotsByHex[hex].map((h) => dcByHotspot[h.address]))
 
     return {
       hex,
       count: hotspotsByHex[hex].length,
       scale: round(rewardScales.length > 0 ? mean(rewardScales) : 0, 2),
+      dc,
     }
   })
 
