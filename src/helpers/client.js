c72b91949d6e64f7ef680e5b5efbaf5ae6273cc2
Daniel James <danielcolinjames@gmail.com>
2021-12-06 19:09:04 -0800

    Increase TAKE_MAX and prepare for >500k hotspots

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,11 +1,10 @@
 const { default: Client, Network } = require('@helium/http')
 
-const TAKE_MAX = 100000
+const TAKE_MAX = 1000000
 const API_BASE_URL = process.env.API_BASE_URL
 
-const client = new Client(
-  new Network({ baseURL: API_BASE_URL, version: 1 }),
-  { retry: 3 },
-)
+const client = new Client(new Network({ baseURL: API_BASE_URL, version: 1 }), {
+  retry: 3,
+})
 
 module.exports = { client, TAKE_MAX, API_BASE_URL }
